3b4e80e3de669fbd7184d83feaa099237f4ba708
lukyg <lukyganteng77@gmail.com>
2022-06-13 22:49:24 +0800

    test

---
+++ src/test/java/com/binar/dummyproject/DummyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.dummyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

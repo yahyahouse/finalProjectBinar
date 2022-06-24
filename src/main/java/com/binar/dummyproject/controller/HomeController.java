abff025b691a69ae1d43c2718fa622611fec9cea
lukyg <lukyganteng77@gmail.com>
2022-06-24 23:00:12 +0800

    Merapikan API

--- src/main/java/com/binar/dummyproject/controller/HomeController.java
+++ src/main/java/com/binar/dummyproject/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.binar.dummyproject.controller;
 import com.binar.dummyproject.model.Product;
 import com.binar.dummyproject.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Home", description = "API for access homepage")
 @RestController
 @RequestMapping("/home-page")
 public class HomeController {

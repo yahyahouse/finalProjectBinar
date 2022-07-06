d66cc3fa161a4047a1a49a392dd5cadb5ebcaf2b
lukyg <lukyganteng77@gmail.com>
2022-07-06 23:28:43 +0800

    Fix : - detail product - offer detail - add response detail (city & username)

--- src/main/java/com/binar/dummyproject/repository/offer/OfferRepository.java
+++ src/main/java/com/binar/dummyproject/repository/offer/OfferRepository.java
@@ -17,6 +17,6 @@ public interface OfferRepository extends JpaRepository<Offer, Long> {
 
     @Query(value = "select offer.offer_id, offer.offer_price, offer.offer_status, offer.product_id, offer.user_id, offer.local_date_time " +
             "from offer inner join(select product_id from product where product.user_id =user_id)p on offer" +
-                    ".product_id =p.product_id; ", nativeQuery = true)
+                    ".product_id =p.product_id ", nativeQuery = true)
     List<Offer> getOfferBySeller(Integer userId, Long productId);
 }

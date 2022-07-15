0d7bc3a997c51893a8664ad4495421dd43b51881
Yahya Wijaya <yahyawijaya082@gmail.com>
2022-07-15 22:21:59 +0700

    user | update password

--- src/main/java/com/binar/dummyproject/controller/UsersController.java
+++ src/main/java/com/binar/dummyproject/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.binar.dummyproject.controller;
 import com.binar.dummyproject.model.UploadResponse;
 import com.binar.dummyproject.model.users.Users;
 import com.binar.dummyproject.model.UsersResponse;
+import com.binar.dummyproject.repository.role.RoleRepository;
 import com.binar.dummyproject.repository.users.UsersRepository;
 import com.binar.dummyproject.service.users.UsersService;
 import com.cloudinary.Cloudinary;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,10 +31,19 @@ import java.util.Map;
 public class UsersController {
 
     @Autowired
-    UsersService usersService;
+    AuthenticationManager authenticationManager;
 
     @Autowired
     UsersRepository usersRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UsersService usersService;
 
 
     /*
@@ -42,30 +54,40 @@ public class UsersController {
             "api_key", "221166829538913",
             "api_secret", "5KfEb789PD2SosIE12zXehlidwM"));
 
-
     @Operation(summary = "users change password")
-    @PostMapping("/public/update-users-password")
-    public ResponseEntity <Map<String, Object>> updateUsersPassword(
-            @RequestBody Map<String, Object> usersPassword){
-        usersService.updateUsersPassword(usersPassword.get("password").toString(), Integer.valueOf(usersPassword.get("userId").toString()));
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/public/update-users-password/{userId}")
+    public ResponseEntity<ResponseEntity> updateUsersPassword(
+            @PathVariable ("userId") Integer userId,
+            @RequestParam String oldPassword,
+            @RequestParam String password,
+            @RequestParam String retypePassword) {
+        Users users = usersRepository.findByUserId(userId);
+        if (password.equals(retypePassword)) {
+            if (passwordEncoder.matches(oldPassword,users.getPassword())){
+                usersService.updateUsersPassword(password,userId);
+                return new ResponseEntity("password berhasil diganti",HttpStatus.OK);
+            } else
+                return new ResponseEntity("Password salah", HttpStatus.BAD_REQUEST);
+
+        } else
+            return new ResponseEntity("Password harus sama", HttpStatus.BAD_REQUEST);
     }
 
     @Operation(summary = "Update users profile")
     @PostMapping(value = "/public/update-users-profile",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> updateUsersProfile(
-            @RequestParam("userId")Integer userId,
-            @RequestParam("username")String username,
+            @RequestParam("userId") Integer userId,
+            @RequestParam("username") String username,
             @RequestParam("users_image") MultipartFile usersImage,
-            @RequestParam("address")String address,
-            @RequestParam("city")String city,
-            @RequestParam("phone")String phone
+            @RequestParam("address") String address,
+            @RequestParam("city") String city,
+            @RequestParam("phone") String phone
     ) throws IOException {
         UploadResponse response = new UploadResponse();
         File file = new File(usersImage.getOriginalFilename());
 
-        try(FileOutputStream os = new FileOutputStream(file)) {
+        try (FileOutputStream os = new FileOutputStream(file)) {
 
             os.write(usersImage.getBytes());
             Map result = cloudinary.uploader().upload(file,
@@ -83,15 +105,15 @@ public class UsersController {
             users.setPhone(phone);
             users.setUrl(url[0]);
             usersRepository.save(users);
-            return new ResponseEntity(new UsersResponse(userId,username,url,address,city,phone), HttpStatus.OK);
-        }catch (Exception e){
+            return new ResponseEntity(new UsersResponse(userId, username, url, address, city, phone), HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @Operation(summary = "Get detail user")
     @GetMapping(value = "/seller/get-user-detail/{userId}")
-    public ResponseEntity<List<Users>> getProductByUserId(@PathVariable("userId") Integer userId){
+    public ResponseEntity<List<Users>> getProductByUserId(@PathVariable("userId") Integer userId) {
         usersService.getUsersByUserId(userId);
         return ResponseEntity.accepted().body(usersService.getUsersByUserId(userId));
     }
